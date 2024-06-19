7be40f5df8b4b10420e845f8b315cab6631a0175
Farid Habib <hsfarid08@gmail.com>
2024-06-19 17:18:14 +0000

    initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#define variables
+number1 = 10
+number2 = 5
+
+#perform operations
+add = number1 + number2
+subtract = number1 - number2
+multiply = number1 * number2
+
+#render the results
+print("Addition of", number1, "and", number2, "is", add)
+print("Subtraction of", number1, "and", number2, "is", subtract)
+print("Multiplication of", number1, "and", number2, "is", multiply)
