d2da6d5054dcd025f951809a39df762c98e55b0c
Farid Habib <hsfarid08@gmail.com>
2024-07-15 17:08:30 +0000

    modify bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,11 +1,11 @@
 class BankAccount:
     def __init__(self, account_balance):
         self.account_balance = account_balance
-        self.initial_balance = 0
+
 
     def deposit(self, amount):
         self.account_balance += amount
-
+        
 
     def withdraw(self, amount):
         if amount < self.account_balance:
@@ -14,7 +14,6 @@ class BankAccount:
         else:
             return False
         
-
+        
     def display_balance(self):
         print(f"Current Balance: $[{self.account_balance}]")
-        
