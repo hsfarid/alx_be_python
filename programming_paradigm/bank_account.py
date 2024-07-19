bfb7a3eee3d2f8e1f2e4aab1fcc1be5e441df6f9
Farid Habib <hsfarid08@gmail.com>
2024-07-19 17:41:43 +0000

    modify bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -18,7 +18,7 @@ class BankAccount:
         
 
     def display_balance(self):
-        print(f"Current Balance: $[{self.account_balance}]")
+        print(f"Current Balance: $[{self.account_balance:.2f}]")
         return True
         
         
