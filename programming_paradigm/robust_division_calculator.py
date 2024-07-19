62626a1210b3ed70acdd9dbf6555f1bc857c06ff
Farid Habib <hsfarid08@gmail.com>
2024-07-19 18:43:56 +0000

    modify robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,8 +1,10 @@
 def safe_divide(numerator, denominator):
-    numerator = float(numerator)
-    denominator = float(denominator)
+    
 
     try:
+        numerator = float(numerator)
+        denominator = float(denominator)
+        
         result = numerator / denominator
         return f"The result of the division is {result}"
     
